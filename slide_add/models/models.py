ed0ced5c54582121ef39726cc0e5f5f485f836ae
huzaifa250 <huz.dark1@gmail.com>
2024-09-10 12:09:01 +0200

    add constrains to gpa which will be enter by user

--- slide_add/models/models.py
+++ slide_add/models/models.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class SlideAdd(models.Model):
     _inherit = 'slide.slide'
-    _description = 'slide_add.slide_add'
+    _description = 'Course with required skills'
 
     is_soft = fields.Boolean(default=True, string='Soft skills complete')
-    gpa = fields.Char('GPA')
+    gpa = fields.Float('GPA', digits=(4, 2), help="Enter the GPA (out of 4.00).")
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.constrains('gpa')
+    def _check_gpa(self):
+        for record in self:
+            if record.gpa < 2.50:
+                raise ValidationError(_("GPA must be 2.50 or higher!"))
