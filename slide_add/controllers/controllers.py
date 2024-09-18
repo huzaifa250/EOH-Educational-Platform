cc94f5e7333bc70f4a660ee542f565b37896bd4c
=huzaifa250 <=huz.dark1@gmail.com>
2024-09-18 21:02:31 +0200

    check if no courses are found to through error

--- slide_add/controllers/controllers.py
+++ slide_add/controllers/controllers.py
@@ -7,7 +7,17 @@ class ElearningController(http.Controller):
     def get_courses(self, **kwargs):
         # get all the courses from the eLearning model
         courses = http.request.env['slide.channel'].sudo().search([])
-        # Pass courses data to the template
+        # Check if no courses are found
+        if not courses:
+            # Render a template for no courses or pass a message to the existing template
+            return http.request.render('slide_add.elearning_courses_template', {
+                'my_courses': [],
+                'error_message': 'No courses available at the moment.'
+            })
+
+        # if courses are found pass courses data to the template
         return http.request.render('slide_add.elearning_courses_template', {
-            'my_courses': courses
+            'my_courses': courses,
+            'error_message': False  # No error, so no message
         })
+
